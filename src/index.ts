cd3d4cb7d26f1b5076e006b57c5f9295336a872d
Raymond <raymond@hetwebbureau.nl>
2021-07-09 19:01:42 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
+import { IProgram, Program, ICommand } from "web-cli-interpreter";
 
+const version = "1.0.0";
+
+export const Todo =  new Program({
+    indentifier: {
+        name: "todo",
+    },
+    version,
+    description:
+        "A program to get information about the curernt command line interface",
+    methods: [
+        {
+            indentifier: {
+                name: "add",
+                abbreviation: "a",
+            },
+            description: "add todo",
+            execute(command: ICommand) {
+                return Promise.resolve('todo info');
+            },
+        },
+        {
+            indentifier: {
+                name: "remove",
+                abbreviation: "r",
+            },
+            description: "remove todo",
+            execute(command: ICommand) {
+                return Promise.resolve('todo info');
+            },
+        },
+        {
+            indentifier: {
+                name: "version",
+                abbreviation: "v",
+            },
+            description: "current installed version",
+            execute(command: ICommand) {
+                return Promise.resolve(`
+                        v${version}
+                    `);
+            },
+        },
+    ],
+});
