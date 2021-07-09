f51d74d70e2ab1fd11357ebd50e271d04ea9368a
Raymond <raymond@hetwebbureau.nl>
2021-07-09 21:14:00 +0200

    feat: version method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+import { Program, ICommand } from "web-cli-interpreter";
 
+const version = "1.0.0";
+
+export const Todo = new Program({
+    indentifier: {
+        name: "todo",
+    },
+    version,
+    description: "Create and manage to-dos",
+    methods: [
+        {
+            indentifier: {
+                name: "version",
+                abbreviation: "v",
+            },
+            description: "current installed version",
+            execute(command: ICommand) {
+                return Promise.resolve(`
+                        v${version}
+                    `);
+            },
+        },
+    ],
+});
